fd10514dad1b97b0d3875cc3e46de569dcd78a31
rafaelrc7 <rafaelrc7@gmail.com>
2022-05-10 13:38:25 -0300

    Add autotools build config and flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "T1 de INF1771 (busca local e A*)";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = import nixpkgs { inherit system; };
+        buscalocal = with pkgs; stdenv.mkDerivation rec {
+          name = "buscalocal";
+          src = ./.;
+          nativeBuildInputs = [ autoreconfHook ];
+        };
+    in {
+
+      defaultPackage = buscalocal;
+
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          gcc
+          gnumake
+          autoconf
+          automake
+          clang-tools
+          gdb
+          valgrind
+        ];
+      };
+    }
+  );
+
+}
+
