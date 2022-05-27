030f662717c855f552a9950cb50782e7dd22427d
rafaelrc7 <rafaelrc7@gmail.com>
2022-05-27 00:47:13 -0300

    Rename genetic algorithm main types and variables

--- src/genetic.c
+++ src/genetic.c
@@ -12,55 +12,55 @@
 
 #define PRINT_GENS
 
-struct solution {
+struct individual {
 	uint8_t genes[STAGES];
 	double time;
 	double fitness;
 };
-typedef struct solution Solution;
+typedef struct individual Individual;
 
 static int validate_genes(const uint8_t genes[STAGES]);
-static int generate_genes(Solution *solutions, const size_t size, const double agilities[CHARS]);
-static void print_solutions(const Solution *const solutions, const size_t size, const size_t cap);
+static int generate_genes(Individual *population, const size_t size, const double agilities[CHARS]);
+static void print_population(const Individual *const population, const size_t size, const size_t cap);
 static double time_stages(const uint8_t genes[STAGES], const double agilities[CHARS]);
-static size_t roulette(const Solution *const solutions, const size_t solutions_num, const double total_fitness);
-static double total_fitness(const Solution *const solutions, size_t solutions_num);
-static double fitness(const Solution solution);
-static size_t cut_solutions(Solution *const solutions, const size_t solutions_cap, const size_t solutions_num);
+static size_t roulette(const Individual *const population, const size_t population_size, const double total_fitness);
+static double total_fitness(const Individual *const population, size_t population_size);
+static double fitness(const Individual solution);
+static size_t cut_population(Individual *const population, const size_t population_cap, const size_t population_size);
 int solution_sort(const void *ptr1, const void *ptr2);
 
 double gen_solve(const double agilities[CHARS], const size_t generation_num,
 				 const size_t pop_step, const size_t pop_cap)
 {
-	Solution *solutions;
-	size_t generation, solutions_num;
+	Individual *population;
+	size_t generation, population_size;
 	double time;
 
-	solutions = (Solution *)calloc(pop_cap + pop_step, sizeof(Solution));
+	population = (Individual *)calloc(pop_cap + pop_step, sizeof(Individual));
 
-	generate_genes(solutions, pop_step, agilities);
-	solutions_num = pop_step;
+	generate_genes(population, pop_step, agilities);
+	population_size = pop_step;
 
-	qsort(solutions, solutions_num, sizeof(Solution), &solution_sort);
+	qsort(population, population_size, sizeof(Individual), &solution_sort);
 
 	for (generation = 0; generation < generation_num; ++generation) {
 		size_t i, j;
-		double total_fitness_val = total_fitness(solutions, solutions_num);
+		double total_fitness_val = total_fitness(population, population_size);
 
 		#ifdef PRINT_GENS
-		printf("\n----- GENERATION %lu (%lu/%lu) -----\n", generation, solutions_num, pop_cap);
-		print_solutions(solutions, solutions_num, 5);
+		printf("\n----- GENERATION %lu (%lu/%lu) -----\n", generation, population_size, pop_cap);
+		print_population(population, population_size, 5);
 		#endif
 
-		for (i = solutions_num; i < solutions_num + pop_step;) {
-			size_t parent1 = roulette(solutions, solutions_num, total_fitness_val);
-			size_t parent2 = roulette(solutions, solutions_num, total_fitness_val);
+		for (i = population_size; i < population_size + pop_step;) {
+			size_t parent1 = roulette(population, population_size, total_fitness_val);
+			size_t parent2 = roulette(population, population_size, total_fitness_val);
 			uint8_t mut;
 
 			for (j = 0; j < STAGES; ++j) {
 				uint8_t select = random_prob(2);
-				solutions[i].genes[j] = select ?
-					solutions[parent1].genes[j] : solutions[parent2].genes[j];
+				population[i].genes[j] = select ?
+					population[parent1].genes[j] : population[parent2].genes[j];
 			}
 
 			mut = random_lim(100);
@@ -68,9 +68,9 @@ double gen_solve(const double agilities[CHARS], const size_t generation_num,
 				size_t id1 = random_lim(STAGES);
 				size_t id2 = random_lim(STAGES);
 
-				uint8_t tmp = solutions[i].genes[id2];
-				solutions[i].genes[id2] = solutions[i].genes[id1];
-				solutions[i].genes[id1] = tmp;
+				uint8_t tmp = population[i].genes[id2];
+				population[i].genes[id2] = population[i].genes[id1];
+				population[i].genes[id1] = tmp;
 			}
 
 			mut = random_lim(100);
@@ -79,12 +79,12 @@ double gen_solve(const double agilities[CHARS], const size_t generation_num,
 				size_t id2 = random_lim(STAGES);
 				uint8_t mask = 1 << (random_lim(CHARS));
 
-				uint8_t tmp = solutions[i].genes[id2] & mask;
-				solutions[i].genes[id2] =
-					(solutions[i].genes[id2] & ~mask)
-					| (solutions[i].genes[id1]);
-				solutions[i].genes[id1] =
-					(solutions[i].genes[id1] & ~mask)
+				uint8_t tmp = population[i].genes[id2] & mask;
+				population[i].genes[id2] =
+					(population[i].genes[id2] & ~mask)
+					| (population[i].genes[id1]);
+				population[i].genes[id1] =
+					(population[i].genes[id1] & ~mask)
 					| tmp;
 			}
 
@@ -95,48 +95,48 @@ double gen_solve(const double agilities[CHARS], const size_t generation_num,
 				uint8_t mask1 = 1 << (random_lim(CHARS));
 				uint8_t mask2 = 1 << (random_lim(CHARS));
 
-				uint8_t tmp1 = solutions[i].genes[id2] & mask1;
-				uint8_t tmp2 = solutions[i].genes[id2] & mask2;
+				uint8_t tmp1 = population[i].genes[id2] & mask1;
+				uint8_t tmp2 = population[i].genes[id2] & mask2;
 
-				solutions[i].genes[id2] =
-					(solutions[i].genes[id2] & ~mask1)
-					| (solutions[i].genes[id1] & mask1);
-				solutions[i].genes[id2] =
-					(solutions[i].genes[id2] & ~mask2)
-					| (solutions[i].genes[id1] & mask2);
+				population[i].genes[id2] =
+					(population[i].genes[id2] & ~mask1)
+					| (population[i].genes[id1] & mask1);
+				population[i].genes[id2] =
+					(population[i].genes[id2] & ~mask2)
+					| (population[i].genes[id1] & mask2);
 
-				solutions[i].genes[id1] =
-					(solutions[i].genes[id1] & ~mask1)
+				population[i].genes[id1] =
+					(population[i].genes[id1] & ~mask1)
 					| tmp1;
-				solutions[i].genes[id1] =
-					(solutions[i].genes[id1] & ~mask2)
+				population[i].genes[id1] =
+					(population[i].genes[id1] & ~mask2)
 					| tmp2;
 			}
 
-			if (validate_genes(solutions[i].genes)) {
-				solutions[i].time = time_stages(solutions[i].genes, agilities);
-				solutions[i].fitness = fitness(solutions[i]);
+			if (validate_genes(population[i].genes)) {
+				population[i].time = time_stages(population[i].genes, agilities);
+				population[i].fitness = fitness(population[i]);
 				++i;
 			}
 		}
 
-		solutions_num += pop_step;
-		if (solutions_num > pop_cap)
-			solutions_num = cut_solutions(solutions, pop_cap, solutions_num);
+		population_size += pop_step;
+		if (population_size > pop_cap)
+			population_size = cut_population(population, pop_cap, population_size);
 	}
 
-	time = solutions[0].time;
+	time = population[0].time;
 
-	free(solutions);
+	free(population);
 
 	return time;
 }
 
 /* TODO: Implement random removal here */
-static size_t cut_solutions(Solution *const solutions, const size_t solutions_cap, const size_t solutions_num) {
-	qsort(solutions, solutions_num, sizeof(Solution), &solution_sort);
+static size_t cut_population(Individual *const population, const size_t population_cap, const size_t population_size) {
+	qsort(population, population_size, sizeof(Individual), &solution_sort);
 
-	return solutions_cap;
+	return population_cap;
 }
 
 static int validate_genes(const uint8_t genes[STAGES]) {
@@ -178,7 +178,7 @@ static size_t generate_possible_genes(struct array **possible_genes) {
 	return array_get_size(*possible_genes);
 }
 
-static int generate_genes(Solution *solutions, const size_t size, const double agilities[CHARS]) {
+static int generate_genes(Individual *population, const size_t size, const double agilities[CHARS]) {
 	struct array *possible_genes;
 	size_t i, j, k, possible_genes_num;
 
@@ -190,18 +190,18 @@ static int generate_genes(Solution *solutions, const size_t size, const double a
 			memcpy(&char_genes[j], array_get(possible_genes, random_lim(possible_genes_num)), sizeof(uint64_t));
 		}
 
-		memset(solutions[i].genes, 0, sizeof(uint8_t) * STAGES);
+		memset(population[i].genes, 0, sizeof(uint8_t) * STAGES);
 
 		for (j = 0; j < STAGES; ++j) {
 			for (k = 0; k < CHARS; ++k) {
 				if (char_genes[k] & (1 << j))
-					solutions[i].genes[j] |= (1 << k);
+					population[i].genes[j] |= (1 << k);
 			}
 		}
 
-		if (validate_genes(solutions[i].genes)) {
-			solutions[i].time = time_stages(solutions[i].genes, agilities);
-			solutions[i].fitness = fitness(solutions[i]);
+		if (validate_genes(population[i].genes)) {
+			population[i].time = time_stages(population[i].genes, agilities);
+			population[i].fitness = fitness(population[i]);
 			++i;
 		}
 	}
@@ -211,7 +211,7 @@ static int generate_genes(Solution *solutions, const size_t size, const double a
 	return 1;
 }
 
-static void print_solutions(const Solution *const solutions, const size_t size, const size_t cap) {
+static void print_population(const Individual *const population, const size_t size, const size_t cap) {
 	size_t i, j, k;
 	char buff[CHARS + 1];
 
@@ -219,12 +219,12 @@ static void print_solutions(const Solution *const solutions, const size_t size, 
 		for (j = 0; j < STAGES; ++j) {
 			uint8_t mask = 1 << (CHARS - 1);
 			for (k = 0; k < CHARS; ++k, mask >>= 1) {
-				buff[k] = solutions[i].genes[j] & mask ? '1' : '0';
+				buff[k] = population[i].genes[j] & mask ? '1' : '0';
 			}
 			buff[k] = 0;
 			printf("%s ", buff);
 		}
-		printf("\t%.06f\n", solutions[i].time);
+		printf("\t%.06f\n", population[i].time);
 	}
 }
 
@@ -253,22 +253,22 @@ static double time_stages(const uint8_t genes[STAGES], const double agilities[CH
 	return time;
 }
 
-static double total_fitness(const Solution *const solutions, size_t solutions_num) {
+static double total_fitness(const Individual *const population, size_t population_size) {
 	size_t i;
 	double sum = 0;
 
-	for (i = 0; i < solutions_num; ++i)
-		sum += solutions[i].fitness;
+	for (i = 0; i < population_size; ++i)
+		sum += population[i].fitness;
 
 	return sum;
 }
 
-static size_t roulette(const Solution *const solutions, const size_t solutions_num, const double total_fitness) {
+static size_t roulette(const Individual *const population, const size_t population_size, const double total_fitness) {
 	size_t i;
 	double r = random() / (double)RAND_MAX;
 
-	for (i = 0; i < solutions_num; ++i) {
-		double pi = solutions[i].fitness / total_fitness;
+	for (i = 0; i < population_size; ++i) {
+		double pi = population[i].fitness / total_fitness;
 		if (r < pi) {
 			return i;
 		}
@@ -278,15 +278,15 @@ static size_t roulette(const Solution *const solutions, const size_t solutions_n
 	return -1;
 }
 
-static double fitness(const Solution solution) {
+static double fitness(const Individual solution) {
 	static const double scale = 10000;
 	return solution.time > 0.0 ?  1.0 / solution.time * scale : DBL_MAX;
 }
 
 int solution_sort(const void *ptr1, const void *ptr2) {
-	if (((Solution *)ptr1)->fitness < ((Solution *)ptr2)->fitness)
+	if (((Individual *)ptr1)->fitness < ((Individual *)ptr2)->fitness)
 		return 1;
-	else if (((Solution *)ptr1)->fitness > ((Solution *)ptr2)->fitness)
+	else if (((Individual *)ptr1)->fitness > ((Individual *)ptr2)->fitness)
 		return -1;
 	else
 		return 0;
