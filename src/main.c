8160dfb52ce448ad30a7f84e93ee9dc73207a157
rafaelrc7 <rafaelrc7@gmail.com>
2022-05-30 20:11:57 -0300

    Tweaks and new minimum

--- src/main.c
+++ src/main.c
@@ -7,9 +7,9 @@
 #include "map.h"
 #include "sdl2_app.h"
 
-#define POP_CAP		2500
-#define POP_STEP	500
-#define GENERATIONS 1100
+#define POP_CAP		1000
+#define POP_STEP	300
+#define GENERATIONS 2100
 
 static void usage(const char *const prog);
 
